2e648876a2aecd20e2927c8aaf4d36f58fddf8a6
Brian E. Granger <ellisonbg@gmail.com>
2017-04-27 14:58:27 -0700

    Updated the init file to work

--- jupyter_alabaster_theme/__init__.py
+++ jupyter_alabaster_theme/__init__.py
@@ -4,22 +4,32 @@ import subprocess
 import sys
 
 
-__version_info__ = (0, 1, 0)
-__version__ = '.'.join(map(str, __version_info__))
-
-
-on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
-
 #---------------------------------------------------------------------------
 # Find packages
 #---------------------------------------------------------------------------
 
+# Set the version of the jupyter_alabaster_theme
+__version_info__ = (0, 1, 0)
+__version__ = '.'.join(map(str, __version_info__))
 
 
+#---------------------------------------------------------------------------
+# Configure the Sphinx Application
+#---------------------------------------------------------------------------
+
+
+
+def get_path():
+    """
+    Shortcut for users whose theme is next to their conf.py.
+    """
+    # Theme directory is defined as our parent directory
+    return os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 
 
 def update_context(app, pagename, templatename, context, doctree):
     context['jupyter_alabaster_theme_version'] = __version__
+
 
 def setup(app):
     app.connect('html-page-context', update_context)
@@ -39,8 +49,4 @@ def setup(app):
 
     return {'version': __version__,
             'parallel_read_safe': True}
-
-
-
-
 
