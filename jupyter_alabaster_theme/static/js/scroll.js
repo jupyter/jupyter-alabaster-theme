86a035129c9aa3d8d02aea656a5624d9dab7c3b6
Brian E. Granger <ellisonbg@gmail.com>
2017-04-27 21:44:51 -0700

    Fixing scroll logic
    * Moving to scroll.js
    * Works with both on page and new pages on load

--- jupyter_alabaster_theme/static/js/scroll.js
+++ jupyter_alabaster_theme/static/js/scroll.js
@@ -18,3 +18,36 @@ $(window).on("scroll", function () {
         // $(".navbar").stop().animate({opacity: 1});
     }
 });
+
+var scroll_to_target = function (target, complete) {
+    var target = $(target);
+    if (target.length) {
+        $('html, body').animate({scrollTop: target.offset().top - 120}, 1, complete);
+    }
+}
+
+// Because we have a fixed position nav bar, when a user clicks on a target with a #hash
+// we have to scroll the page a bit so the target is visible.
+$(window).on("load", function () {
+    // Handle the case where we are loading a page with a #id target in the URL
+    var hash = window.location.hash;
+    if (hash) {
+        scroll_to_target(hash);
+    }
+
+    // Handle the case when the user clicks on a #id target on the current page
+    $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            var hash = this.hash;
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            scroll_to_target(target, function () {
+                location.hash = hash;
+            });
+            if (target.length) { return false; }
+        }
+    });
+});
+
+
+
